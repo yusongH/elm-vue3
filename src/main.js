76156605f24a349fd55b99535302cab9168ea308
yusongH <549962989@qq.com>
2021-09-11 16:17:50 +0800

    feat: 引入vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import store from '@/store'
 
 const app = createApp(App)
 
 // 路由
 app.use(router)
+// veux
+app.use(store)
 
 // 挂载
 app.mount('#app')
