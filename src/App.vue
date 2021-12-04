03e37f7d7cf6469e3e222178712be9a90006c5d6
yusongH <549962989@qq.com>
2021-12-04 17:01:00 +0800

    feat: 增加svg,后期需要优化

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <loading></loading>
   <router-view></router-view>
+  <icon-svg></icon-svg>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import Loading from '@/components/common/Loading.vue'
+import IconSvg from '@/components/common/Svg.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    Loading
+    Loading,
+    IconSvg
   }
 })
 </script>
