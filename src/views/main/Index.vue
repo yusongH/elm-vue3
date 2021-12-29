35c2850822e840ee4cc029a73b6ed04b59708175
yusongH <549962989@qq.com>
2021-12-29 22:11:18 +0800

    fix: 底部搜索按钮跳转搜索页带上参数,否则会报404

--- src/views/main/Index.vue
+++ src/views/main/Index.vue
@@ -4,11 +4,23 @@
   </div>
   <van-tabbar route>
     <van-tabbar-item replace to="/home" icon="home-o">外卖</van-tabbar-item>
-    <van-tabbar-item replace to="/search" icon="search">搜索</van-tabbar-item>
+    <van-tabbar-item replace :to="`/search/${home.geohash}`" icon="search"
+      >搜索</van-tabbar-item
+    >
     <van-tabbar-item replace to="/order" icon="orders-o">订单</van-tabbar-item>
     <van-tabbar-item replace to="/my" icon="user-o">我的</van-tabbar-item>
   </van-tabbar>
 </template>
+
+<script setup>
+import { toRefs } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+console.log('store', store)
+const { home } = toRefs(store.state)
+</script>
 
 <style lang="less" scoped>
 .main-box {
