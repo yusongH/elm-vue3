1f2dd02240bd6f7fb7ea8d59f2fe48c290b61361
yusongH <549962989@qq.com>
2021-12-30 22:26:54 +0800

    fix: 去掉底部按钮跳转的替换当前页面历史

--- src/views/main/Index.vue
+++ src/views/main/Index.vue
@@ -3,12 +3,12 @@
     <router-view></router-view>
   </div>
   <van-tabbar route>
-    <van-tabbar-item replace to="/home" icon="home-o">外卖</van-tabbar-item>
-    <van-tabbar-item replace :to="`/search/${home.geohash}`" icon="search"
+    <van-tabbar-item to="/home" icon="home-o">外卖</van-tabbar-item>
+    <van-tabbar-item :to="`/search/${home.geohash}`" icon="search"
       >搜索</van-tabbar-item
     >
-    <van-tabbar-item replace to="/order" icon="orders-o">订单</van-tabbar-item>
-    <van-tabbar-item replace to="/my" icon="user-o">我的</van-tabbar-item>
+    <van-tabbar-item to="/order" icon="orders-o">订单</van-tabbar-item>
+    <van-tabbar-item to="/my" icon="user-o">我的</van-tabbar-item>
   </van-tabbar>
 </template>
 
